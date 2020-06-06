782ccdd67d7b0ba1745f49bff1c34baa01696149
zukaishi <ishizuka215@gmail.com>
2020-06-06 22:35:40 +0900

    update

--- pokemon_data_create.rb
+++ pokemon_data_create.rb
@@ -7,10 +7,26 @@ DATA_URL="https://wiki.xn--rckteqa2e.com/wiki/%E3%83%9D%E3%82%B1%E3%83%A2%E3%83%
 html = URI.open(DATA_URL).read
 doc = Nokogiri::HTML.parse(html)
 list = doc.css('.mw-parser-output table.sortable tbody tr td:nth-child(2)>a').map(&:content)
+
+# Todo 一行で完了させる
+list.map!{|x| x.rindex("♂")? x.gsub("♂","オス") : x}
+list.map!{|x| x.rindex("♀")? x.gsub("♀","メス") : x}
 puts JSON.pretty_generate(list.uniq)
 
-	
+# Todo 開始、終了となるポケモンの名前を受け取る
+# Todo　ァなどの文字を、アに置き換える　list + 受け取った２つの名前を
+# Todo 伸ばし棒は、棒の一つ前の文字を最後の文字として考える処理
+# Todo ンで終わっているポケモンを除外する
+# Todo 開始ポケモンの最後の文字を、最初とするポケモンを探し出す
+# Todo これを終了ポケモンになるまで繰り返す
+# Todo　終了ポケモンになったら、そのパターンを保持する
+# Todo パターンを変えて、再度検索を行い、保持していたパターンより数が少なければ置き換え
+# Todo もっとも最短となるパターンが残るはずなので、そのパターンを配列を表示して完了
+
+=begin	
 File.open("pokemon_names.json", mode = "w"){|f|
   f.write(list)
 }
+=end
+
 
