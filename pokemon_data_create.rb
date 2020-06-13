583809bd6aa0ab2c932e70df5b17a7820b3c063e
zukaishi <ishizuka215@gmail.com>
2020-06-13 15:27:21 +0900

    update

--- pokemon_data_create.rb
+++ pokemon_data_create.rb
@@ -9,6 +9,9 @@ html = URI.open(DATA_URL).read
 doc = Nokogiri::HTML.parse(html)
 pokemon_list = doc.css('.mw-parser-output table.sortable tbody tr td:nth-child(2)>a').map(&:content)
 
+# ンで終わるポケモンを除外する
+pokemon_list = pokemon_list.reject {|v| v[-1] == "ン"}
+
 # Todo 一行で完了させる
 pokemon_list.map!{|x| x.rindex("♂")? x.gsub("♂","オス") : x}
 pokemon_list.map!{|x| x.rindex("♀")? x.gsub("♀","メス") : x}
@@ -18,19 +21,19 @@ pokemon_list.map!{|x| x.rindex("ィ")? x.gsub("ィ","イ") : x}
 pokemon_list.map!{|x| x.rindex("ゥ")? x.gsub("ゥ","ウ") : x}
 pokemon_list.map!{|x| x.rindex("ェ")? x.gsub("ェ","エ") : x}
 pokemon_list.map!{|x| x.rindex("ォ")? x.gsub("ォ","オ") : x}
-puts JSON.pretty_generate(pokemon_list.uniq)
-p pokemon_list
+#puts JSON.pretty_generate(pokemon_list.uniq)
 
 # 開始、終了となるポケモンの名前を受け取る
 puts "ポケモン２匹を半角スペース区切りで入力してください"
 pokemons = gets.split(' ')
 # Todo 処理を綺麗にする
 if !pokemon_list.rindex( pokemons[0] ) or !pokemon_list.rindex( pokemons[1] ) then
-  p exit
+#  p exit
 end
 
 # Todo 伸ばし棒は、棒の一つ前の文字を最後の文字として考える処理
 # Todo ンで終わっているポケモンを除外する
+
 # Todo 開始ポケモンの最後の文字を、最初とするポケモンを探し出す
 # Todo これを終了ポケモンになるまで繰り返す
 # Todo　終了ポケモンになったら、そのパターンを保持する
