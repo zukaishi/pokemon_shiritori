c65816af64756611b3eaad6991accb40578e4df1
zukaishi <ishizuka215@gmail.com>
2020-07-05 16:43:04 +0900

    update

--- pokemon_data_create.rb
+++ pokemon_data_create.rb
@@ -52,55 +52,58 @@ if shirotori_mode
   end
   start_p = pokemons[0]
   end_p = pokemons[1]
+
+  # 開始、終了のポケモンを除外する
+  pokemon_list = pokemon_list.reject {|v| v == start_p}
+  pokemon_list = pokemon_list.reject {|v| v == end_p}
+
+  # スタートポケモンを操作用の変数に格納
+  target_p = start_p
+  puts "start"
+
+  # しりとり開始
+  for i in 0...pokemon_list.count
+    puts target_p
+
+    # 最後の伸ばし棒が最後にある場合一つ前の文字を最後の文字とする
+    last_str =  target_p[-1]
+    if last_str == "ー"
+      last_str =  target_p[-2]
+    end
+
+    # 全体のリストから最初の文字が対象の最後の文字と一致するものを探し出してリストを作る
+    pokemon_list2 = pokemon_list.reject {|v| v[0] != last_str}
+    count = pokemon_list2.count
+    if count == 0
+      puts "### しりとり負け ###"
+      break
+    end
+
+    # 候補一覧を表示する
+    #puts JSON.pretty_generate(pokemon_list2.uniq)
+    # ターゲットを決める
+    target_p = pokemon_list2[rand(0...count)]
+
+    # 対象となったポケモンを大元のリストから除外する
+    pokemon_list = pokemon_list.reject {|v| v == target_p}
+
+
+    # 終了ポケモンの最初の文字と、最後の文字が一致してた場合しりとり終了
+    if end_p[0] == last_str
+      puts end_p
+      # puts last_str
+      puts "end"
+      break;
+    end
+  end
 else
   puts "検索したい最初の文字を１文字入力してください"
   serach_word = STDIN.gets
   p serach_word
 end
 
-# 開始、終了のポケモンを除外する
-pokemon_list = pokemon_list.reject {|v| v == start_p}
-pokemon_list = pokemon_list.reject {|v| v == end_p}
-
-# スタートポケモンを操作用の変数に格納
-target_p = start_p
-puts "start"
-
-# しりとり開始
-for i in 0...pokemon_list.count
-  puts target_p
-
-  # 最後の伸ばし棒が最後にある場合一つ前の文字を最後の文字とする
-  last_str =  target_p[-1]
-  if last_str == "ー"
-    last_str =  target_p[-2]
-  end
-
-  # 全体のリストから最初の文字が対象の最後の文字と一致するものを探し出してリストを作る
-  pokemon_list2 = pokemon_list.reject {|v| v[0] != last_str}
-  count = pokemon_list2.count
-  if count == 0
-    puts "### しりとり負け ###"
-    break
-  end
-
-  # 候補一覧を表示する
-  #puts JSON.pretty_generate(pokemon_list2.uniq)
-  # ターゲットを決める
-  target_p = pokemon_list2[rand(0...count)]
-
-  # 対象となったポケモンを大元のリストから除外する
-  pokemon_list = pokemon_list.reject {|v| v == target_p}
 
 
-  # 終了ポケモンの最初の文字と、最後の文字が一致してた場合しりとり終了
-  if end_p[0] == last_str
-    puts end_p
-    # puts last_str
-    puts "end"
-    break;
-  end
-end
 
 # Todo オプションを渡して、しりとりモードか、検索モードできるように
 # Todo 検索モードの場合は、最初の文字か、最後の文字か選択できるようにして文字を一文字渡す形にする
