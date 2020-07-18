6b4f3a9a3317d1661d6fb644923a7142dc6af71d
zukaishi <ishizuka215@gmail.com>
2020-07-18 17:59:17 +0900

    update

--- pokemon_data_create.rb
+++ pokemon_data_create.rb
@@ -10,25 +10,11 @@ require 'nokogiri'
 require 'json'
 require 'optparse'
 require './utils/help'
+require './utils/pokemon_list'
 
 opt = OptionParser.new
 optHelp(opt)
-
-# ポケモン一覧を取得する
-pokemon_data_file = "./data/pokemon_list.csv"
-if File.exist?(pokemon_data_file)
-  f = File.open(pokemon_data_file)
-    html = f.read 
-  f.close
-else
-  DATA_URL="https://wiki.xn--rckteqa2e.com/wiki/%E3%83%9D%E3%82%B1%E3%83%A2%E3%83%B3%E4%B8%80%E8%A6%A7"
-  html = URI.open(DATA_URL).read
-  File.open(pokemon_data_file, mode = "w"){|f|
-    f.write(html)
-  }
-end
-doc = Nokogiri::HTML.parse(html)
-pokemon_list = doc.css('.mw-parser-output table.sortable tbody tr td:nth-child(2)>a').map(&:content)
+pokemon_list = pokemon_list()
 
 # モードを決定
 shirotori_mode = true
---
+++ utils/pokemon_list.rb
@@ -0,0 +1,17 @@
+# utils/pokemon_list.rb
+def pokemon_list()
+    # ポケモン一覧を取得する
+    pokemon_data_file = "./data/pokemon_list.csv"
+    if File.exist?(pokemon_data_file)
+        f = File.open(pokemon_data_file)
+            html = f.read 
+        f.close
+    else
+        html = URI.open("https://wiki.xn--rckteqa2e.com/wiki/%E3%83%9D%E3%82%B1%E3%83%A2%E3%83%B3%E4%B8%80%E8%A6%A7").read
+        File.open(pokemon_data_file, mode = "w"){|f|
+            f.write(html)
+        }
+    end
+    doc = Nokogiri::HTML.parse(html)
+    pokemon_list = doc.css('.mw-parser-output table.sortable tbody tr td:nth-child(2)>a').map(&:content)
+end
