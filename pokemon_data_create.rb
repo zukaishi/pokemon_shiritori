cee312b18a612be3c9ce9b3b95e74f25ebc0256d
zukaishi <ishizuka215@gmail.com>
2020-07-19 11:36:35 +0900

    update

--- pokemon_data_create.rb
+++ pokemon_data_create.rb
@@ -6,26 +6,13 @@
 require 'json'
 require 'optparse'
 require './utils/help'
+require './utils/mode'
 require './utils/pokemon_list'
 
 opt = OptionParser.new
 optHelp(opt)
+shirotori_mode, search_pos = modeSelect(opt)
 pokemon_list = pokemon_list()
-
-# モードを決定
-shirotori_mode = true
-search_pos = 0
-opt.on('-s', '--start', 'add an item') {
-  puts '最初の文字検索モード'
-  shirotori_mode = false
-  search_pos = 0
-}
-opt.on('-e', '--end', 'add an item') {
-  puts '最後の文字検索モード'
-  shirotori_mode = false
-  search_pos = -1
-}
-opt.parse(ARGV)
 
 # しりとりモードならンで終わるポケモンを除外する
 if shirotori_mode
---
+++ utils/mode.rb
@@ -0,0 +1,18 @@
+# utils/mode.rb
+def modeSelect(opt)
+    # モードを決定
+    shirotori_mode = true
+    search_pos = 0
+    opt.on('-s', '--start', 'add an item') {
+    puts '最初の文字検索モード'
+    shirotori_mode = false
+    search_pos = 0
+    }
+    opt.on('-e', '--end', 'add an item') {
+    puts '最後の文字検索モード'
+    shirotori_mode = false
+    search_pos = -1
+    }
+    opt.parse(ARGV)
+    return shirotori_mode,search_pos
+end
